8f097b19a14dd1ee1f7ba54cc74476ba9bc83e50
Guilherme Müller <gmullernh@outlook.com>
2020-04-19 14:57:38 -0300

    Sample project.

---
+++ Pages/HTML.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StaticComponentGenerator
+{
+    public class HTMLModel : PageModel
+    {
+        /// <summary>
+        /// Where the returned content is passed to the view.
+        /// </summary>
+        public string RequestedContent { get; set; }
+
+        public async Task OnGet()
+        {
+            PuppeteerSharpController.PuppeteerController puppeteerController = new PuppeteerSharpController.PuppeteerController();
+            RequestedContent = await puppeteerController.GenerateContent(
+                "ComponentOne", 
+                "Test", 
+                new PuppeteerSharpController.Strategies.Download.StrategyHTML()
+            );
+        }
+    }
+}
